9acc8717f919ba3fbd060bf0a6dc3648df437d0d
LoreAlv <83510170+LoreAlv@users.noreply.github.com>
2024-04-17 09:08:18 +0200

    26. RED-GREEN validacion email

--- 01_user_management/src/pages/login-page/login-page.test.jsx
+++ 01_user_management/src/pages/login-page/login-page.test.jsx
@@ -4,6 +4,15 @@ import { LoginPage } from "./login-page";
 const getSubmitBtn = () => {
     return screen.getByRole("button", { name: /submit/i });
 };
+
+const getEmailInput = () => {
+    return screen.getByLabelText(/email/i);
+};
+
+const getPasswordInput = () => {
+    return screen.getByLabelText(/password/i);
+};
+
 describe("pruebas de login-page", () => {
     test("it should render login title", () => {
         render(<LoginPage />);
@@ -16,9 +25,9 @@ describe("pruebas de login-page", () => {
         render(<LoginPage />);
         // screen.debug();
         //miramos que esté el campo email ignore-case
-        expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
+        expect(getEmailInput()).toBeInTheDocument();
         //miramos que esté el campo password ignore-case
-        expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+        expect(getPasswordInput()).toBeInTheDocument();
         //miramos que esté el botón submit ignore-case
         expect(getSubmitBtn()).toBeInTheDocument();
     });
@@ -33,5 +42,18 @@ describe("pruebas de login-page", () => {
         // screen.debug();
         expect(screen.getByText(/The email is required/i)).toBeInTheDocument();
         expect(screen.getByText(/The password is required/i)).toBeInTheDocument();
+    });
+
+    test("should validate the email format", async () => {
+        render(<LoginPage />);
+        await act(() => {
+            //type an invalid email
+            fireEvent.change(getEmailInput(), { target: { value: "a" } });
+            //submit del form
+            fireEvent.submit(getSubmitBtn());
+        });
+        //validar los errores
+        // screen.debug();
+        expect(screen.getByText(/The email is not valid/i)).toBeInTheDocument();
     });
 });
