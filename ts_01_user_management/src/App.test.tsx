929d0df930e474131251f5176beb21fb5b841acd
LoreAlv <83510170+LoreAlv@users.noreply.github.com>
2024-04-24 07:37:24 +0200

    34. testeo de app.ts

--- ts_01_user_management/src/App.test.tsx
+++ ts_01_user_management/src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import App from './App';
+import { renderWithProviders } from './mocks/render-with-providers';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  renderWithProviders(<App />);
+  expect(screen.getByText(/login/i)).toBeInTheDocument();
 });
--- ts_01_user_management/src/App.tsx
+++ ts_01_user_management/src/App.tsx
@@ -10,24 +10,10 @@ const queryClient = new QueryClient()
 function App() {
   return (
     <div className="App">
-          <QueryClientProvider client={queryClient}>
-      <CssBaseline />
-      <LoginPage />
-    </QueryClientProvider>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
+        <LoginPage />
+      </QueryClientProvider>
     </div>
   );
 }
