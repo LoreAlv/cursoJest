18927ce01afee5b342ffb6a2a6f5ad871df4730d
LoreAlv <83510170+LoreAlv@users.noreply.github.com>
2024-04-24 15:03:36 +0200

    41. validar inputs form

--- 02-store-app/src/form/form.js
+++ 02-store-app/src/form/form.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import {TextField, InputLabel, Select} from '@mui/material'
 
 function Form() {
   //   console.log('hola')
   return (
     <>
       <h1>Create product</h1>
-      <div />
+      <form>
+        <TextField label="name" id="name" />
+        <TextField label="size" id="size" />
+        <InputLabel htmlFor="type">Type</InputLabel>
+        <Select
+          native
+          labelId="type"
+          id="type"
+          value=""
+          label="type"
+          inputProps={{name: 'type', id: 'type'}}
+          //   onChange={handleChange}
+        >
+          <option value="" aria-label="none" />
+          <option value="electronic">Electronic</option>
+          <option value="furniture">Furniture</option>
+          <option value="clothing">Clothing</option>
+        </Select>
+      </form>
     </>
   )
 }
--- 02-store-app/src/form/form.test.js
+++ 02-store-app/src/form/form.test.js
@@ -10,4 +10,21 @@ describe('pruebas de form.js', () => {
       screen.getByRole('heading', {name: /create product/i}),
     ).toBeInTheDocument()
   })
+
+  test('should esist fields: name, size, type(electronic, furniture, clothing)', () => {
+    render(<Form />)
+
+    screen.debug()
+    // existen los campos
+
+    expect(screen.getByLabelText(/name/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/size/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/type/i)).toBeInTheDocument()
+
+    // existen las opciones
+
+    expect(screen.queryByText(/electronic/i)).toBeInTheDocument()
+    expect(screen.queryByText(/furniture/i)).toBeInTheDocument()
+    expect(screen.queryByText(/clothing/i)).toBeInTheDocument()
+  })
 })
