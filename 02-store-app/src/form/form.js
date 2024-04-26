c1604de35c161e876705905663206d8c2f6de9f1
LoreAlv <83510170+LoreAlv@users.noreply.github.com>
2024-04-26 09:21:03 +0200

    43. validaciones al submit

--- 02-store-app/src/form/form.js
+++ 02-store-app/src/form/form.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {TextField, InputLabel, Select, Button} from '@mui/material'
 
 function Form() {
-  //   console.log('hola')
+  const [formErrors, setFormErrors] = useState({name: '', size: '', type: ''})
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    const {name, size, type} = event.target.elements
+    const formAux = {...formErrors}
+    if (!name.value || name.value.trim().length === 0)
+      formAux.name = 'The name is required'
+    if (!size.value || size.value.trim().length === 0)
+      formAux.size = 'The size is required'
+    if (!type.value) formAux.type = 'The type is required'
+    setFormErrors(formAux)
+  }
   return (
     <>
       <h1>Create product</h1>
-      <form>
-        <TextField label="name" id="name" />
-        <TextField label="size" id="size" />
+      <form onSubmit={handleSubmit}>
+        <TextField label="name" id="name" helperText={formErrors.name} />
+        <TextField label="size" id="size" helperText={formErrors.size} />
         <InputLabel htmlFor="type">Type</InputLabel>
         <Select
           native
@@ -24,7 +36,8 @@ function Form() {
           <option value="furniture">Furniture</option>
           <option value="clothing">Clothing</option>
         </Select>
-        <Button>Submit</Button>
+        {formErrors.type.length && <p>{formErrors.type}</p>}
+        <Button type="submit">Submit</Button>
       </form>
     </>
   )
--- 02-store-app/src/form/form.test.js
+++ 02-store-app/src/form/form.test.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import {render, screen} from '@testing-library/react'
+import {fireEvent, render, screen, waitFor} from '@testing-library/react'
 import Form from './form'
 
-describe('pruebas de form.js', () => {
+const getSubmitButton = () => screen.getByRole('button', {name: /submit/i})
+const getNameField = () => screen.getByLabelText(/name/i)
+const getSizeField = () => screen.getByLabelText(/size/i)
+const getTypeField = () => screen.getByLabelText(/type/i)
+
+describe('pruebas de  carga de form.js', () => {
   test('should render la pagina', () => {
     render(<Form />)
     // expect(screen.queryByText(/create product/i)).toBeInTheDocument()
@@ -17,9 +22,9 @@ describe('pruebas de form.js', () => {
     // screen.debug()
     // existen los campos
 
-    expect(screen.getByLabelText(/name/i)).toBeInTheDocument()
-    expect(screen.getByLabelText(/size/i)).toBeInTheDocument()
-    expect(screen.getByLabelText(/type/i)).toBeInTheDocument()
+    expect(getNameField()).toBeInTheDocument()
+    expect(getSizeField()).toBeInTheDocument()
+    expect(getTypeField()).toBeInTheDocument()
 
     // existen las opciones
 
@@ -32,8 +37,25 @@ describe('pruebas de form.js', () => {
     render(<Form />)
 
     // screen.debug()
-    // existen los campos
+    // existen el botón
 
-    expect(screen.getByRole('button', {name: /submit/i})).toBeInTheDocument()
+    expect(getSubmitButton()).toBeInTheDocument()
+  })
+})
+
+describe('when the form is submited', () => {
+  test('should show "The [field name] is required" if the field is empty', () => {
+    // The [field name] is required
+    render(<Form />)
+    expect(screen.queryByText(/the name is required/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/the size is required/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/the type is required/i)).not.toBeInTheDocument()
+
+    fireEvent.click(getSubmitButton())
+    screen.debug()
+
+    expect(screen.queryByText(/the name is required/i)).toBeInTheDocument()
+    expect(screen.queryByText(/the size is required/i)).toBeInTheDocument()
+    expect(screen.queryByText(/the type is required/i)).toBeInTheDocument()
   })
 })
